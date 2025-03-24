7e507d4dc81fd614b010f71e39fea579a1a58d4a
muarts <muartsokucu@gmail.com>
2025-03-24 21:52:04 +0300

    add input locators of add contact page

--- src/pages/add-contact.page.ts
+++ src/pages/add-contact.page.ts
@@ -4,12 +4,36 @@ import { LoginPage } from './login.page';
 export class AddContactPage {
     readonly page: Page;
     readonly logoutButton: Locator;
+    readonly firstNameInput: Locator;
+    readonly lastNameUnput: Locator;
     readonly dateOfBirthInput: Locator;
+    readonly emailInput: Locator;
+    readonly phoneInput: Locator;
+    readonly streetAddressOneInput: Locator;
+    readonly streetAddressTwoInput: Locator;
+    readonly cityInput: Locator;
+    readonly stateProvinceInput: Locator;
+    readonly postalCodeInput: Locator;
+    readonly countryInput: Locator;
+    readonly submitButton: Locator;
+    readonly cancelButton: Locator;
 
     constructor (page: Page) {
         this.page = page;
         this.logoutButton = page.locator('[id="logout"]');
         this.dateOfBirthInput = page.locator('[id="birthdate"]');
+        this.firstNameInput = page.locator('[id="firstName"]');
+        this.lastNameUnput = page.locator('[id="lastName"]');
+        this.emailInput = page.locator('[id="email"]');
+        this.phoneInput = page.locator('[id="phone"]');
+        this.streetAddressOneInput = page.locator('[id="street1"]');
+        this.streetAddressTwoInput = page.locator('[id="street2"]');
+        this.cityInput = page.locator('[id="city"]');
+        this.stateProvinceInput = page.locator('[id="stateProvince"]');
+        this.postalCodeInput = page.locator('[id="postalCode"]');
+        this.countryInput = page.locator('[id="country"]');
+        this.submitButton = page.locator('[id="submit"]');
+        this.cancelButton = page.locator('[id="cancel"]');
     }
 
     async go () {
